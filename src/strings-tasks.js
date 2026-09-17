47a119295412aafe069e32dbbae2c7e4d2e47ace
vanmik <ivap2006@gmail.com>
2026-09-17 20:34:22 +0500

    feat: add 22 function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -395,8 +395,21 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+    if (char.toLowerCase() !== char.toUpperCase()) {
+      if (char === char.toUpperCase()) {
+        result += char.toLowerCase();
+      } else {
+        result += char.toUpperCase();
+      }
+    } else {
+      result += char;
+    }
+  }
+  return result;
 }
 
 /**
