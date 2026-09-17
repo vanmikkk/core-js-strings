3637695f4b3494ec42e1efa19e86f81fe7e6ae31
vanmik <ivap2006@gmail.com>
2026-09-17 16:25:39 +0500

    feat: add 18 function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -307,8 +307,15 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = 'aeiouyAEIOUY';
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    if (vowels.includes(str[i])) {
+      sum += 1;
+    }
+  }
+  return sum;
 }
 
 /**
