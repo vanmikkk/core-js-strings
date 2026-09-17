108e356f130dc3a7567bf5764543e972d9e0689a
vanmik <ivap2006@gmail.com>
2026-09-17 15:47:50 +0500

    feat: add 5 function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -83,8 +83,8 @@ function getFirstChar(value) {
  *   removeLeadingAndTrailingWhitespaces('cat ') => 'cat'
  *   removeLeadingAndTrailingWhitespaces('\t\t\tHello, World! ') => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingAndTrailingWhitespaces(value) {
+  return value.trim();
 }
 
 /**
