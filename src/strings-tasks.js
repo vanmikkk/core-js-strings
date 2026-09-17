02779abdf657afd98d1493a974b7809a31f27ab8
vanmik <ivap2006@gmail.com>
2026-09-17 16:39:06 +0500

    feat: add 20 function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -351,8 +351,15 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const wordsArray = sentence.split(' ');
+  let theLongestWord = '';
+  for (let i = 0; i < wordsArray.length; i += 1) {
+    if (theLongestWord.length < wordsArray[i].length) {
+      theLongestWord = wordsArray[i];
+    }
+  }
+  return theLongestWord;
 }
 
 /**
