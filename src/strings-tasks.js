2aa1444cb9a87c68a71070fb798e90a317017eb8
vanmik <ivap2006@gmail.com>
2026-09-17 15:56:03 +0500

    feat: add 8 function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -130,8 +130,12 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (times < 0) {
+    return '';
+  }
+
+  return str.repeat(times);
 }
 
 /**
