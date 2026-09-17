98aeca3662906bfd28dc696cd19060719998daa3
vanmik <ivap2006@gmail.com>
2026-09-17 15:50:27 +0500

    feat: add 7 function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -113,8 +113,8 @@ function removeLeadingWhitespaces(value) {
  *   removeTrailingWhitespaces('cat ') => 'cat'
  *   removeTrailingWhitespaces('\t\t\tHello, World! ') => '\t\t\tHello, World!'
  */
-function removeTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeTrailingWhitespaces(value) {
+  return value.trimEnd();
 }
 
 /**
