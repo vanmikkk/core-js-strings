54190eccfccdb96fbe20f7060c9b7a909f7d903e
vanmik <ivap2006@gmail.com>
2026-09-17 16:11:27 +0500

    feat: add 11 function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -190,8 +190,15 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (!str) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str.charCodeAt(i);
+  }
+  return sum;
 }
 
 /**
